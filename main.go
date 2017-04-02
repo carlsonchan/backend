571562cfe34e8714190f43c624a0b2e44a44e6f8
Jeffrey Leung <jleung513@gmail.com>
2017-04-01 17:33:22 -0700

    Update code to latest version (last one used in nwHacks)

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
     "encoding/json"
     "log"
     "net/http"
-    "strings"
+    _ "strings"
+   "time"
     //"database/sql"
 
-    // "fmt"
+    _ "fmt"
     "github.com/gorilla/mux"
     "github.com/jinzhu/gorm"
     _ "github.com/lib/pq"
@@ -17,10 +18,13 @@ type Patient struct{
   Gender int
   Id,Name,Dob,Address string
 }
+type EmergencyContact struct{
+  Id, Pid, Name, Phone string
+}
 type Person struct {
     ID        string   `json:"id,omitempty"`
     Information   *Information `json:"information,omitempty"`
-    Emergencycontact  *Emergencycontact   `json:"emergencycontact,omitempty"`
+    EmergencyContacts  []EmergencyContact   `json:"emergency_contacts,omitempty"`
     HistoryArray []HistoryInfo `json:"historyarray,omitempty"`
 }
 
@@ -37,10 +41,10 @@ type Birth struct {
   Year int `json:"year,omitempty"`
 }
 
-type Emergencycontact struct {
-  Econtact string   `json:"econtact,omitempty"`
-  Phone string   `json:"phone,omitempty"`
-}
+// type Emergencycontact struct {
+//   Econtact string   `json:"econtact,omitempty"`
+//   Phone string   `json:"phone,omitempty"`
+// }
 
 type HistoryArray struct{
   Collection []HistoryInfo `json:"historyarray"`
@@ -71,39 +75,57 @@ var patient []Person
 func GetPatientEndpoint(w http.ResponseWriter, req *http.Request) {
     params := mux.Vars(req)
 
-    db, err := gorm.Open("postgres", "postgresql://janitor_dev@ip-172-31-6-7.us-west-2.compute.internal:26257?sslcert=/home/ubuntu/certs/janitor_dev.cert&sslkey=/home/ubuntu/certs/janitor_dev.key")
+    // db, err := gorm.Open("postgres", "host=ip-172-31-6-7.us-west-2.compute.internal:26257 user=janitor_dev dbname=nwhacks sslmode=enable sslcert=/home/ubuntu/certs/janitor_dev.cert sslkey=/home/ubuntu/certs/janitor_dev.key")
+
+    db, err := gorm.Open("postgres", "postgresql://janitor_dev@ip-172-31-6-7.us-west-2.compute.internal:26257/NWHACKS?sslcert=/home/ubuntu/certs/janitor_dev.cert&sslkey=/home/ubuntu/certs/janitor_dev.key")
     if err != nil {
       log.Fatalf("error connection to the database: %s", err)
     }
 
     var raw_pat Patient
-    db.Table("nwhacks.patients").Where("id = ?", params["id"]).Find(&raw_pat)
+    db.Table("patients").Where("id = ?", params["id"]).Find(&raw_pat)
 
     var gen string
-    if(raw_pat == 0){
+    if(raw_pat.Gender == 0){
       gen = "M"
-    }
-    else if(raw_pat == 1){
+    } else if(raw_pat.Gender == 1){
       gen = "F"
-    }
-    else{
+    } else {
       gen = "O"
     }
 
-    var DateStr string
-    t, err := time.Parse(raw_pat, "2011-01-19")
-    fmt.Printf("%s", t)
-    fmt.Printf("%s", err)
+    t, err := time.Parse(raw_pat.Dob, "2011-01-19")
+    birth := &Birth{
+          Month: int(t.Month()),
+          Day: int(t.Day()),
+          Year: t.Year(),
+        }
 
+    var emer EmergencyContact
+    var emers []EmergencyContact
+    // db.Table("nwhacks.patients").Where("id = ?", params["id"]).Find(&pat)
+    // db.Model(&pat).Related(&emer, "EmergencyContact")
+    
+    db.Find(&emers)
+    db.Table("NWHACKS.emergency_contacts").Where("pid = ?", raw_pat.Id).Find(&emer)
 
-    ymd
     var patient Person
-    patient = append(patient, Person{ID: raw_pat.Id,
-      Information &Information{Fullname: raw_pat.Name, Gender: gen, Address: raw_pat.Address, Birth &Birth }})
 
-    // fmt.Printf("%d", item.ID)
-    // fmt.Printf("%s", pat.Id)
-    json.NewEncoder(w).Encode(raw_pat)
+    patient = Person{ID: raw_pat.Id,
+      Information: &Information{
+        Fullname: raw_pat.Name,
+        Gender: gen,
+        Address: raw_pat.Address,
+        Birth: birth,
+        },
+        EmergencyContacts: emers,
+
+      }
+
+    // // fmt.Printf("%d", item.ID)
+    // // fmt.Printf("%s", pat.Id)
+    json.NewEncoder(w).Encode(patient)
+    // json.NewEncoder(w).Encode(emer)
      defer db.Close()
     // json.NewEncoder(w).Encode(patient)
 }
